32881a09e8324d302cd747a3a2b18c5c7015b12a
pljhonglu <pljhonglu@126.com>
2018-10-06 22:11:20 +0800

    修复 account 验证问题

--- fava/static/javascript/codemirror/mode-beancount.js
+++ fava/static/javascript/codemirror/mode-beancount.js
@@ -73,7 +73,7 @@ CodeMirror.defineSimpleMode('beancount', {
       token: 'date',
     },
     {
-      regex: /(?:[A-Z][A-Za-z0-9\-]+)(?::[A-Z][A-Za-z0-9\-]*)+/,
+      regex: /(?:[A-Z][^\s]+)(?::[A-Z][^\s]*)+/,
       token: 'account',
     },
     {
